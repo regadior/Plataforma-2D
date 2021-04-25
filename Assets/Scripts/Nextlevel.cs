d5be0e5f8b8401e9ca795f051221643893666ee9
regadior <regadior@gmail.com>
2021-04-25 12:55:26 +0200

    Aniamcion de caida + Double Jump

--- Assets/Scripts/Nextlevel.cs
+++ Assets/Scripts/Nextlevel.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Nextlevel : MonoBehaviour
-{   //Si se toca la caja de colision de el objeto con el Player, provoca que se cambie a una nueva escena
+
+{   
+
+    
+    
+    //Si se toca la caja de colision de el objeto con el Player, provoca que se cambie a una nueva escena
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,6 +9,10 @@ public class PlayerMove : MonoBehaviour
 
     //Se le da valor a una variable
     public float jumpSpeed = 3;
+    //Se le da valor a una variable
+    public float doubleJumpSpeed = 2.5f;
+    //Variable para ver si podemos hacer un segundo salto
+    private bool canDoubleJump;
 
     //Se crea un nuevo nombre para referirse a Rigidbody2D
     Rigidbody2D rb2D;
@@ -34,6 +38,61 @@ public class PlayerMove : MonoBehaviour
         rb2D = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+            //Si se pulsa el espacio se chequea si la caja de colisiones esta tocando el suelo, si es asi salta, si no es asi no se acciona
+        if (Input.GetKey("space"))
+        {
+            if (CheckGround.isGrounded)
+            {
+                canDoubleJump = true;   //si es true entonces 
+                rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+                animator.SetBool("Run", false);
+            }
+            else
+            {
+                if (Input.GetKeyDown("space")) //Hace que cuando saltemos y volvamos a pulsar espacio se ejequte el doble salto
+                {
+                    if (canDoubleJump)
+                    {
+                        animator.SetBool("DoubleJump",true);
+                        rb2D.velocity = new Vector2(rb2D.velocity.x, doubleJumpSpeed); // Igual que es salto normal pero con la velocidad de ptra var
+                        canDoubleJump = false; //una vez echo se desactiva el doble salto
+
+                    }
+                }
+            }
+        }
+
+        if (CheckGround.isGrounded==false)
+        {
+             animator.SetBool("Jump", true);
+            animator.SetBool("Run", false);
+            
+         }
+        if (CheckGround.isGrounded==true)
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
+            animator.SetBool("Falling", false);
+         }
+           
+           if (rb2D.velocity.y<0)
+           {
+               animator.SetBool("Falling",true);
+           }
+           else if (rb2D.velocity.y>0)
+           {
+               animator.SetBool("Falling",false);
+           }
+        
+
+
+     
+
+    }
+
+
     void FixedUpdate()
     {      //Si se pulsa la d se mueve en sentido positivo del eje x
         if (Input.GetKey("d"))
@@ -53,21 +112,7 @@ public class PlayerMove : MonoBehaviour
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
             animator.SetBool("Run", false);
         }
-            //Si se pulsa el espacio se chequea si la caja de colisiones esta tocando el suelo, si es asi salta, si no es asi no se acciona
-        if (Input.GetKey("space") && CheckGround.isGrounded)
-        {
-            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
-            animator.SetBool("Run", false);
-        }
-        if (CheckGround.isGrounded==false)
-        {
-            animator.SetBool("Jump", true);
-            animator.SetBool("Run", false);
-        }
-        if (CheckGround.isGrounded==true)
-        {
-            animator.SetBool("Jump", false);
-        }
+        
         //Se crea una opción para poder modificar los valores de el salto y poder hacer medio y salto completo
         if (betterJump)
         {
