423a816efd1d34241da85d949ca211902ef5e100
regadior <regadior@gmail.com>
2021-04-25 20:15:49 +0200

    Level Menu

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpenDoor : MonoBehaviour
+{
+   
+    public Text text;
+    public string levelName;
+    private bool inDoor = false;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+            inDoor = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(false);
+            inDoor = false;
+
+        }
+
+    }
+
+
+    private void Update()
+    {
+        if (inDoor && Input.GetKey("e"))
+        {
+            SceneManager.LoadScene(levelName);
+        }
+
+    }
+
+
+
+}
